88eca7f15ea9009dc3497c3a23a6699d7ead53f6
Tim Westerhuis <tim.westerhuis@hotmail.com>
2023-12-22 12:48:43 +0100

    first push

---
+++ ShoppingList/src/test/java/com/workshopgit/ShoppingList/ShoppingListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshopgit.ShoppingList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
